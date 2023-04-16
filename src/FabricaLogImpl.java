4f7125d440fe7a7b3decadcecae14642dd27352e
ByAmando <125359276+ByAmando@users.noreply.github.com>
2023-04-16 17:10:56 +0200

    Ficheros para LOG

---
+++ src/FabricaLogImpl.java
@@ -0,0 +1,18 @@
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.*;
+
+class FabricaLogImpl extends UnicastRemoteObject implements FabricaLog {
+	List<ServicioLog> l;
+	FabricaLogImpl () throws RemoteException {
+		l = new LinkedList<ServicioLog>(); 
+	}
+	public ServicioLog crearLog (Log log) throws RemoteException{
+		ServicioLog s = new ServicioLogImpl(log);
+		l.add(s); 
+		return s;
+	}
+    //public List<ServicioLog> obtenerLogs() throws RemoteException{
+    //		return l;
+    //	}
+}
